56efd87ea73d6ec52462edd037dc42e77ca1da8d
Luis Aneudy <dluisaneudy82@gmail.com>
2023-03-22 07:29:05 -0400

    Improved Function, solve errors

--- src/Helpers/Helpers.js
+++ src/Helpers/Helpers.js
@@ -1,6 +1,7 @@
 export const handleScroll = e => {
     e.preventDefault()
-    const seccionScroll = e.target.attributes.href.value;
+    const seccionScroll = e.target.attributes.href?.value || '#Home';
+    console.log(seccionScroll)
     const seccion = document.querySelector(seccionScroll)
     seccion.scrollIntoView({ behavior: 'smooth' });
 }
